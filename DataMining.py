9ddf16cd88c22fce78190d9c1c51fc57e3a10c60
achristopher4 <alexchristopher1999@yahoo.com>
2023-01-15 21:33:49 -0500

    Created file and starting to develop program

--- DataMining.py
+++ DataMining.py
@@ -1,2 +1,16 @@
 ## Weather Data Extraction
-##
+## Date: 1/15/2023
+
+## Website: https://www.wunderground.com/history/monthly/us/pa/state-college
+
+
+from selenium import webdriver
+import selenium
+import pandas as pd
+import numpy as np
+
+
+
+
+## Source Point
+source = "https://www.wunderground.com/history/monthly/us/pa/state-college/KUNV/date/2022-1" 
