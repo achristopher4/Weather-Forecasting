83ffa73c44d93fb195db9e6a9d24aea2aec682ed
achristopher4 <alexchristopher1999@yahoo.com>
2023-03-16 12:27:02 -0400

    merging the normalized datasets back together

--- TFNet/train_model.py
+++ TFNet/train_model.py
@@ -184,21 +184,23 @@ moving_avg = 5
 single_level = train_data[train_data['level'] == 1000][['lat', 'lon', 'time', 'u10', 'v10', 't2m', 'tisr', 'tcc', 'tp']]
 preserved_single = single_level.head(moving_avg)
 single_level = single_level.reset_index().set_index('time').groupby(['lat', 'lon']).rolling(window=moving_avg).mean()
-print(preserved_single)
-print(single_level.head(10))
-print('\n\n')
+#print(preserved_single)
+#print(single_level.head(10))
+#print('\n\n')
 
 ## Split data by level, lat, and log --> normalize each by their respective location, level, and time --> put back together
 multi_level = train_data[train_data['level'] == 1000][['lat', 'lon', 'time', 'level', 'z', 'pv', 'r', 'q', 't', 'u', 'vo', 'v']]
 preserved_multi = multi_level.head(moving_avg)
 multi_level = multi_level.reset_index().set_index('time').groupby(['lat', 'lon', 'level']).rolling(window=moving_avg).mean()
-print(preserved_multi)
-print(multi_level.head(10))
+#print(preserved_multi)
+#print(multi_level.head(10))
 
-## Add u10, v10, t2m, tisr, tcc, tp back into the dataset
+## merging the normalized datasets back together
 
-train_data = train_data[start_val_date > data["time"]]
-validation_data = train_data[(start_val_date <= data["time"]) & (data["time"] <= end_val_date)]
+
+
+#train_data = train_data[start_val_date > data["time"]]
+#validation_data = train_data[(start_val_date <= data["time"]) & (data["time"] <= end_val_date)]
 ## Drop cos sin day and year from validation dataset
 
 
